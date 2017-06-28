90a15180cb3de150522a53e8e6ded0b8948fa02a
hne <huangnuoen@gmail.com>
2017-06-28 10:46:32 +0800

    配置gulp环境

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,41 @@
 //引入gulp
 var gulp = require('gulp');
 //引入组件
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+var jshint = require('gulp-jshint'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-minify-css'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	livereload = require('gulp-livereload'),
+	notify = require('gulp-notify');
 //检查脚本
-gulp.task('lint',function(){
+gulp.task('js',function(){
 	gulp.src('src/js/*.js')
 		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('dist/js'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglifyj())
+		.pipe(gulp.dest('dist/js'))
+		pipe(notify({message: 'js task complete'}));
 });
 //编译sass
 gulp.task('sass', function() {
 	gulp.src('src/scss/*.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('src/css'));
+		.pipe(sass({style: 'expanded'}))
+		.pipe(autoprefixer('last 2 version', 'safair 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest('src/css'))
+		.pipe(notify({message: 'sass complete'}));
 });
-//scripts压缩合并，重命名
-gulp.task('scripts', function() {
-	gulp.src('src/js/*.js')
-		.pipe(concat('all.js'))
-		.pipe(gulp.dest('dist/js'))
-		.pipe(rename('all.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'));
-})
+//自动加载
 //watch任务
 gulp.task('watch', function() {
 	gulp.watch('src/scss/**/*.scss', ['sass']); 
-})
+	gulp.watch('src/js/**/*.js', ['js']);
+	livereload.listen();
+	gulp.watch(['dist/**']).on('change', livereload.changed);
+});
 //
 //
