c7f815d0c3a16b089a732518b07d23cc2053c806
hne <huangnuoen@gmail.com>
2017-07-27 17:09:16 +0800

    add 自动省略样式

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+/*$('p.ellipsis').each(function(){
+	var maxWidth = 40;
+	if($(this).text().length > maxWidth) {
+		$(this).text($(this).text().substring(0, maxWidth));
+		$(this).html($(this).html() + '…');
+	}
+})
+*/function maxWidth() {
+	
+}
