4142be21a12d27d11f1776453c6d34322c0384eb
huangnuoen <1261290560@qq.com>
2017-08-01 23:32:12 +0800

    class

--- es6/server/public/js/index.js
+++ es6/server/public/js/index.js
@@ -67,6 +67,10 @@
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	{
@@ -90,6 +94,75 @@
 
 		var a = new Point('pretty', 'girl');
 		console.log(a.toString());
+	}
+	{
+		var Parent = function () {
+			function Parent() {
+				var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'es6';
+
+				_classCallCheck(this, Parent);
+
+				this.name = name;
+			}
+
+			_createClass(Parent, [{
+				key: 'longName',
+				get: function get() {
+					return 'first Name' + this.name;
+				},
+				set: function set(value) {
+					this.name = value;
+				}
+			}]);
+
+			return Parent;
+		}();
+
+		var Child = function (_Parent) {
+			_inherits(Child, _Parent);
+
+			function Child() {
+				var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'child';
+
+				_classCallCheck(this, Child);
+
+				//即可传入参数
+				var _this = _possibleConstructorReturn(this, (Child.__proto__ || Object.getPrototypeOf(Child)).call(this, name));
+
+				_this.type = 'son';
+				return _this;
+			}
+
+			return Child;
+		}(Parent);
+
+		var v = new Parent('v');
+		console.log(new Child());
+		console.log(v.longName);
+		v.longName = 'hello';
+		console.log(v.longName);
+	}
+	{
+		var _Parent2 = function () {
+			function _Parent2() {
+				var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'es6';
+
+				_classCallCheck(this, _Parent2);
+
+				this.name = name;
+			}
+
+			_createClass(_Parent2, null, [{
+				key: 'tell',
+				value: function tell() {
+					console.log('tell');
+				}
+			}]);
+
+			return _Parent2;
+		}();
+
+		_Parent2.tell();
 	}
 
 /***/ })
