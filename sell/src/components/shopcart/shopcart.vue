972aa99cbcdbea75d91cf58ac129e8976c57d8cd
huangnuoen <1261290560@qq.com>
2017-08-15 22:56:48 +0800

    shopcart.vue

--- sell/src/components/shopcart/shopcart.vue
+++ sell/src/components/shopcart/shopcart.vue
@@ -6,11 +6,14 @@
 					<div class="logo">
 						<i class="icon-shopping_cart"></i>
 					</div>
+					<div class="num">{{count}}</div>
 				</div>
-				<div class="price">￥</div>
+				<div class="price">￥{{totalPrice}}元</div>
 				<div class="desc">另需配送费{{deliveryPrice}}元</div>
 			</div>
-			<div class="content-right"></div>
+			<div class="content-right">
+				<div class="pay">￥{{minPrice}}元起送</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -18,6 +21,12 @@
 <script>
 	export default {
 		props: {
+			selectFoods: {
+				type: Array,
+				default() {
+					return [];
+				}
+			},
 			deliveryPrice: {
 				type: Number,
 				default: 0
@@ -25,6 +34,22 @@
 			minPrice: {
 				type: Number,
 				default: 0
+			}
+		},
+		computed: {
+			totalPrice() {
+				let total = 0;
+				this.selectFoods.forEach((food) => {
+					total += food.price * food.count;
+				});
+				return total;
+			},
+			totalCount() {
+				let count = 0;
+				this.selectFoods.forEach((food) => {
+					count += food.count;
+				});
+				return count;
 			}
 		}
 	};
@@ -44,6 +69,7 @@
 			display: flex
 			background: #141d27
 			font-size: 0
+			color: #80858a
 			.content-left
 				flex: 1
 				.logo-wrapper
@@ -58,16 +84,33 @@
 					vertical-align: top
 					border-radius: 50%
 					background: #141d27
+					.num
+						position: absolute
+						top: 0
+						right: 0
+						width: 24px
+						height: 16px
+						line-height: 16px
+						text-align: center
+						border-radius: 16px
+						font-size: 9px
+						font-weight: 700
+						color: #fff
+						background: rgb(240, 20, 20)
+						box-shadow: 0 4px 8px 0 rgba(0, 0, 0, .4)
 					.logo
 						width: 100%
 						height: 100%
 						border-radius: 50%
 						background: #2b343c
 						text-align: center
+						&.highlight
+							background: RGB(0, 160, 220)
 						.icon-shopping_cart
 							line-height: 44px
 							font-size: 24px
-							color: #80858a
+							&.highlight
+								color: #fff
 				.price
 					display: inline-block
 					margin-top: 12px
@@ -78,15 +121,20 @@
 					padding-right: 12px
 					box-sizing: border-box
 					border-right: 1px solid rgba(255, 255, 255, .1)
-					color: #80858a
 				.desc
 					display: inline-block
 					vertical-align: top
 					margin: 12px 0 0 12px
 					line-height: 24px
-					color: #80858a
 					font-size: 16px
 			.content-right
 				flex: 0 0 105px
 				width: 105px
+				.pay
+					height: 48px
+					line-height: 48px
+					text-align: center
+					font-size: 12px
+					background: #2b343c
+					font-weight: 700
 </style>
