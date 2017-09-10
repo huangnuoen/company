1fadc03767adbab91e1a9de5f7d6df97a8a2606c
huangnuoen <1261290560@qq.com>
2017-09-10 23:24:00 +0800

    player.vue

---
+++ vue-music/src/components/player/player.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="player">
+  	<div class="normal-player">播放器</div>
+  	<div class="mini-player"></div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
