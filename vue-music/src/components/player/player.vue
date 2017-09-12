a973dc3bbbcc610d8d8dab0d495901f962cdff82
huangnuoen <1261290560@qq.com>
2017-09-12 22:06:48 +0800

    player.vue的显示，样式，数据获取

--- vue-music/src/components/player/player.vue
+++ vue-music/src/components/player/player.vue
@@ -2,20 +2,20 @@
   <div class="player" v-show="playlist.length>0">
   	<div class="normal-player" v-show="fullScreen">
   		<div class="background">
-  			<img src="" width="100%" height="100%">
+  			<img :src="currentSong.image" width="100%" height="100%">
   		</div>
   		<div class="top">
-  			<div class="back">
+  			<div class="back" @click="back">
   				<i class="icon-back"></i>
   			</div>
-  			<h1 class="title">1</h1>
-  			<h2 class="subtitle">1</h2>
+  			<h1 class="title" v-html="currentSong.name"></h1>
+  			<h2 class="subtitle" v-html="currentSong.singer"></h2>
   		</div>
   		<div class="middle">
   			<div class="middle-l">
   				<div class="cd-wrapper">
   					<div class="cd">
-  						<img src="" alt="" class="image">
+  						<img :src="currentSong.image" class="image">
   					</div>
   				</div>
   				<div class="playing-lyric-wrapper">
@@ -49,13 +49,13 @@
   			</div>
   		</div>
   	</div>
-  	<div class="mini-player" v-show="!fullScreen">
+  	<div class="mini-player" v-show="!fullScreen" @click="open">
   		<div class="icon">
-  			<img src="" width="40" height="40">
+  			<img :src="currentSong.image" width="40" height="40">
   		</div>
   		<div class="text">
-  			<h2 class="name"></h2>
-  			<p class="desc"></p>
+  			<h2 class="name"v-html="currentSong.name"></h2>
+  			<p class="desc" v-html="currentSong.singer"></p>
   		</div>
   		<div class="control">
   			<i class="icon-playlist"></i>
@@ -65,13 +65,25 @@
 </template>
 
 <script>
-	import {mapGetters} from 'vuex'
+	import {mapGetters, mapMutations} from 'vuex'
   export default {
   	computed: {
   		...mapGetters([
   			'fullScreen',
-  			'playlist'
+  			'playlist',
+  			'currentSong'
   		])
+  	},
+  	methods: {
+  		back() {
+  			this.setFullScreen(false)
+  		},
+  		open() {
+  			this.setFullScreen(true)
+  		},
+  		...mapMutations({
+  			setFullScreen: 'SET_FULL_SCREEN'
+  		})
   	}
   }
 </script>
@@ -104,7 +116,7 @@
 				.back
 					position: absolute
 					top: 0
-					left: 60px
+					left: 6px
 					z-index: 50
 					.icon-back
 						display: block
@@ -130,7 +142,98 @@
 				width: 100%
 				top: 80px
 				bottom: 170px
-				white-space:ｎｏｗｒａｐ
+				white-space: nowrap
+				font-size: 0
+				.middle-l
+					display: inline-block
+					vertical-align: top
+					position: relative
+					width: 100%
+					height: 0
+					padding-top: 80%
+					.cd-wrapper
+						position: absolute
+						left: 10%
+						top: 0
+						width: 80%
+						height: 100%
+						.cd
+							width: 100%
+							height: 100%
+							box-sizing: border-box
+							border: 10px solid rgba(255, 255, 255, .2)
+							border-radius: 50%
+							.image
+								position: absolute
+								left: 0
+								top: 0
+								width: 100%
+								height: 100%
+								border-radius: 50%
+				.middle-r
+					display: inline-block
+					vertical-align: top
+					width: 100%
+					height: 100%
+					overflow: hidden
+			.bottom
+				position: absolute
+				bottom: 50px
+				width: 100%
+				.dot-wrapper
+					text-align: center
+					font-size: 0
+					.dot
+						display: inline-block
+						vertical-align: middle
+						margin: 0 4px
+						width: 8px
+						height: 8px
+						border-radius: 50%
+						background: $color-text-l
+						&.active
+							width: 20px
+							border-radius: 5px
+							background: $color-text-ll
+				.progress-wrapper
+					display: flex
+					align-items: center
+					width: 80%
+					margin: 0px auto
+					padding: 10px 0
+					.time
+						color: $color-text
+						font-size: $font-size-small
+						flex: 0 0 30px
+						line-height: 30px
+						width: 30px
+						&.time-l
+							text-align: left
+						&.time-r
+							text-align: right
+					.progress-bar-wrapper
+						flex: 1
+				.operators
+					display: flex
+					align-items: center
+					.icon
+						flex: 1
+						color: $color-theme
+						&.disable
+							color: $color-theme-d
+						i
+							font-size: 30px
+					.i-left
+						text-align: right
+					.i-center
+						padding: 0 20px
+						text-align: center
+						i
+							font-size: 40px
+					.i-right
+						text-align: left
+					.icon-favorite
+						color: $color-sub-theme
 		.mini-player
 			display: flex
 			align-items: center
@@ -141,5 +244,47 @@
 			width: 100%
 			height: 60px
 			background: $color-highlight-background
+			&.mini-enter-active, &.mini-leave-active
+				transition: all 0.4s
+			&.mini-enter, &.mini-leave-to
+				opacity: 0
+			.icon
+				flex: 0 0 40px
+				width: 40px
+				padding: 0 10px 0 20px
+				img
+					border-radius: 50%
+					&.play
+						animation: rotate 10s linear infinite
+					&.pause
+						animation-play-state: paused
+			.text
+				display: flex
+				flex-direction: column
+				justify-content: center
+				flex: 1
+				line-height: 20px
+				overflow: hidden
+				.name
+					margin-bottom: 2px
+					no-wrap()
+					font-size: $font-size-medium
+					color: $color-text
+				.desc
+					no-wrap()
+					font-size: $font-size-small
+					color: $color-text-d
+			.control
+				flex: 0 0 30px
+				width: 30px
+				padding: 0 10px
+				.icon-play-mini, .icon-pause-mini, .icon-playlist
+					font-size: 30px
+					color: $color-theme-d
+				.icon-mini
+					font-size: 32px
+					position: absolute
+					left: 0
+					top: 0
 
 </style>
