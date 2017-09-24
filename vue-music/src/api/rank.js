24241a97c7111a078ad72760e92b96408ea3de81
huangnuoen <1261290560@qq.com>
2017-09-24 16:04:53 +0800

    排行榜单详情页

--- vue-music/src/api/rank.js
+++ vue-music/src/api/rank.js
@@ -1,6 +1,7 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 
+// 获取各个歌单
 export function getTopList() {
 	const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg?'
 
@@ -8,6 +9,21 @@ export function getTopList() {
 		platform: 'h5',
 		needNewCode: 1,
 		g_tk: 1617356337
+	})
+
+	return jsonp(url, data, options)
+}
+
+// 获取歌单详情
+export function getMusicList(topid) {
+	const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg?'
+
+	const data = Object.assign({}, commonParams, {
+		topid,
+		page: 'detail',
+		type: 'top',
+		platform: 'h5',
+		needNewCode: 1
 	})
 
 	return jsonp(url, data, options)
--- vue-music/src/components/top-list/top-list.vue
+++ vue-music/src/components/top-list/top-list.vue
@@ -1,24 +1,59 @@
 <template>
 	<transition name="slider">
-    <music-list :title="title" :bgImage="bgImage"></music-list>
+    <music-list :title="title" :bgImage="bgImage" :songs="songs"></music-list>
 	</transition>
 </template>
 
 <script>
   import MusicList from 'components/music-list/music-list'
   import {mapGetters} from 'vuex'
-
+  import {getMusicList} from 'api/rank'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
   export default {
+    data() {
+      return {
+        songs: []
+      }
+    },
     computed: {
       title() {
         return this.topList.title
       },
       bgImage() {
-        return this.topList.picUrl
+        if (this.songs.length) {
+          return this.songs[0].image
+        }
+        return ''
       },
       ...mapGetters([
         'topList'
       ])
+    },
+    created() {
+      this._getMusicList()
+    },
+    methods: {
+      _getMusicList() {
+        if (!this.topList.id) {
+          this.$router.push('/rank')
+        }
+        getMusicList(this.topList.id).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.songlist)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((item) => {
+          const musicData = item.data
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
     },
     components: {
       MusicList
