66bbfdabdd75fede5680884f10c7ebd32b11df59
huangnuoen <1261290560@qq.com>
2018-04-10 22:18:49 +0800

    小程序

--- ReaderAndMovie/pages/welcome/welcome.js
+++ ReaderAndMovie/pages/welcome/welcome.js
@@ -1,3 +1,16 @@
 Page({
-  
+  onTap: function (event) {
+    wx.redirectTo({
+      url: '../posts/post',
+      success: function (res) {},
+      fail: function () {},
+      complete: function () {}
+    });
+  },
+  onTextTap(event) {
+
+  },
+  onUnload: function () {
+    // console.log('unload')
+  }
 })
