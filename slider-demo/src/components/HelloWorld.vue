995f3259e6ff2635ea051b4f9d8126ebf04b3e70
huangnuoen <1261290560@qq.com>
2018-07-17 21:48:30 +0800

    增加动效；改变样式；限制出界

--- slider-demo/src/components/HelloWorld.vue
+++ slider-demo/src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
-<template>
-  <div class="slider" @touchstart="sliderStart" @touchmove.stop.prevent="sliderMove">
+       <template>
+  <div class="slider"  @touchstart="sliderStart" @touchmove.stop.prevent="sliderMove">
     <div class="box" ref="box" :data-index="index" v-for="(item,index) in arr" :class="{'active':currentIndex==index}"></div>
   </div>
 </template>
@@ -9,7 +9,7 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      arr: new Array(20),
+      arr: new Array(50),
       currentIndex: 0,
       touch: {}
     };
@@ -24,15 +24,20 @@ export default {
       this.scrollTo(this.touch.anchorIndex);
     },
     sliderMove(e) {
-      let boxWidth=this.$refs.box[0].clientWidth;
+      let boxWidth = document.documentElement.clientWidth/ this.$refs.box.length;
+      // let boxWidth = this.$refs.box[0].clientWidth;
       let firstT = e.touches[0];
       this.touch.x2 = firstT.clientX;
-      let delta=(this.touch.x2-this.touch.x1)/boxWidth;
-      let index=parseInt(this.touch.anchorIndex)+Math.round(delta)
+      let delta = (this.touch.x2 - this.touch.x1) / boxWidth;
+      let index = parseInt(this.touch.anchorIndex) + Math.round(delta);
       this.scrollTo(index);
-      
     },
     scrollTo(i) {
+      if(i<0) {
+        i=0;
+      } else if(i>=this.$refs.box.length) {
+        i = this.$refs.box.length-1
+      }
       this.currentIndex = i;
     }
   }
@@ -42,15 +47,23 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
 .slider {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
   width: 100%;
   height: 20px;
   display: flex;
+  border-bottom: 4px solid red;
   .box {
-    width: 5%;
-    height: 100%;
+    width: .8%;
+    height: 80%;
+      transition: all .4s;
     background-color: pink;
     &.active {
+      height: 100%;
+      // transform: scaleY(2);
       background-color: red;
+      transition: all .4s;
     }
   }
 }
